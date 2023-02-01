b18bdb5096d28b19d4fb98c2cb5a5690664fa030
Christian Anderson <christian@techonomics.org>
2023-02-01 08:40:20 -0800

    Update ml_backend.py

--- ml_backend.py
+++ ml_backend.py
@@ -8,7 +8,7 @@ class ml_backend:
         """Returns a generated an email using GPT3 with a certain prompt and starting sentence"""
 
         response = openai.Completion.create(
-        engine="davinci",
+        engine="text-davinci-003",
         prompt=userPrompt + "\n\n" + start,
         temperature=0.71,
         max_tokens=150,
